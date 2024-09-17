00268acfbc973cf494a5a17f3eacd6a66a3b11a2
Syafiq1111 <syafiqhafizh06@gmail.com>
2024-09-16 22:51:20 -0700

    up

--- Post-Test/Post-Test-2/2409106009_SyafiqHafizhFarizi_A1'24_posttest2.py
+++ Post-Test/Post-Test-2/2409106009_SyafiqHafizhFarizi_A1'24_posttest2.py
@@ -12,5 +12,5 @@ modulus_nim = int(str(nim)[-3:])
 print (f"Nama lengkap saya adalah {nama_lengkap}, biasanya saya dipanggil {nama_panggilan},"
         f"nim saya adalah {nim}, saya berumur {umur} pada tahun ini, "
         f"saya berasal dari {asal_daerah} pada tahun ini saya keterima di universitas mulawarman prodi {prodi},"
-        f"dan target ipk saya untuk lulus dari universitas ini yaitu {ipk}  ")
+        f"dan target ipk saya untuk lulus dari universitas ini yaitu {ipk}.  ")
 print (f" Nim terakhir saya yaitu {modulus_nim}.")
