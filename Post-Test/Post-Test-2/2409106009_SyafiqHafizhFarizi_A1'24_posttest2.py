503e73cecf7798ae0710589a9c26f1016a6bcb90
Syafiq1111 <syafiqhafizh06@gmail.com>
2024-09-16 22:45:35 -0700

    first commit

---
+++ Post-Test/Post-Test-2/2409106009_SyafiqHafizhFarizi_A1'24_posttest2.py
@@ -0,0 +1,16 @@
+nama_lengkap = input("nama lengkap= ")
+nama_panggilan = input("panggilan= ")
+nim = int(input("nim= "))
+umur = input ("umur= ")
+hobi = input("hobi= ")
+asal_daerah = input("asal daerah= ")
+prodi = input("prodi= ")
+ipk = float(input("ipk= "))
+
+modulus_nim = int(str(nim)[-3:]) 
+
+print (f"Nama lengkap saya adalah {nama_lengkap}, biasanya saya dipanggil {nama_panggilan},"
+        f"nim saya adalah {nim}, saya berumur {umur} pada tahun ini, "
+        f"saya berasal dari {asal_daerah} pada tahun ini saya keterima di universitas mulawarman prodi {prodi},"
+        f"dan target ipk saya untuk lulus dari universitas ini yaitu {ipk}  ")
+print (f" Nim terakhir saya yaitu {modulus_nim}.")
